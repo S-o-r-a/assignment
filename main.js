a0ba8228c94ff0a8d6abb7c5c8ea762cab017644
Jiyoo <42067334+S-o-r-a@users.noreply.github.com>
2022-10-07 19:41:06 +0800

    dasda

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ window.onload = function(){
     }
 
     bgTile.onload = function(){
-        for(var i = 0; i < 38; i++){
+       /* for(var i = 0; i < 38; i++){
             c.drawImage(bgTile,49,0,16,16,0+(50*i),0(50*i),64,64);
-            }
+            }*/*
+            grass();
     }
 }
-
 
